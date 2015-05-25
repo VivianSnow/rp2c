bebd72652056ea3789d020ea6f2d230341068e83
VivianSnow <726608501@qq.com>
2015-05-25 21:29:19 +0800

    V5版本发布啦
    正确的处理了argv。

--- gen/rp2cMain.py
+++ gen/rp2cMain.py
@@ -5,10 +5,21 @@ from rp2cParser import rp2cParser
 from rp2cListener import rp2cListener
 import sys
 
-#def main(argv):
-def main():
+def main(argv):
+#def main():
     #sys.stderr = open("error.txt", "w")
-    input = FileStream("input.txt")
+    if len(argv) == 1:
+        print u"you fu**ing idiot, please use -help to catch help info"
+        return 
+    elif len(argv) == 2 and argv[1] == "-help":
+        print u"Use like this: rp2cMain.py source.pas dest.cpp"
+        return
+    elif len(argv) == 2:
+        input = FileStream(argv[1])
+    elif len(argv) == 3:
+        input = FileStream(argv[1])
+        sys.stdout = open(argv[2], "w")
+    #input = FileStream("input.txt")
     lexer = rp2cLexer(input)
     stream = CommonTokenStream(lexer)
     parser = rp2cParser(stream)
@@ -17,5 +28,5 @@ def main():
     walker.walk(rp2cListener(), tree)
 
 if __name__ == '__main__':
-    #main(sys.argv)
-    main()
+    main(sys.argv)
+    #main()
